d2aa50d28c18c446bc1e381a7919ad74fb59c7ab
McRx71 <50086322+McRx71@users.noreply.github.com>
2020-07-23 21:13:10 +0200

    new stuff

---
+++ SR/AutoSR.py
@@ -0,0 +1,25 @@
+import pyautogui, os, time
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+
+def selectChamp():
+    clickImage('random.bmp')
+    
+
+
+
+def clickImage(imagepath):
+    buttonBox = (None, None, None, None)
+    while not all(buttonBox):
+        print(buttonBox, imagepath)
+        buttonBox = pyautogui.locateOnScreen(imagepath, grayscale=True)
+        if buttonBox == None:
+            buttonBox = (None, None, None, None)
+    print(buttonBox)
+    buttonpoint = pyautogui.center(buttonBox)
+    pyautogui.mouseDown(buttonpoint, button='left');
+    pyautogui.mouseUp(buttonpoint, button='left')
+    print(buttonpoint)
