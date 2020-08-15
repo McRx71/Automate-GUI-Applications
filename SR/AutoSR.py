4437b0dbc987999fffbd324bf01b01803691b231
Chris <dragon707@gmx.de>
2020-08-15 02:18:38 +0200

    added classes

--- SR/AutoSR.py
+++ SR/AutoSR.py
@@ -5,68 +5,80 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
+class championAssets:
+    def __init__(self, champ):
+        self.champ = champ
 
-def clickImage(imagepath):
-    time.sleep(0.1)
-    buttonBox = (None, None, None, None)
-    while not all(buttonBox):
-        print(buttonBox, imagepath)
-        buttonBox = pyautogui.locateOnScreen(imagepath, grayscale=False, confidence=0.6)
-        if buttonBox == None:
-            buttonBox = (None, None, None, None)
-    print(buttonBox)
-    buttonpoint = pyautogui.center(buttonBox)
-    pyautogui.click(buttonpoint, button='left')
 
-    print(buttonpoint)
+    def dlSquareAssets(self):
+        self.champ = self.champ.capitalize()
+        url = 'http://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/' + self.champ + '.png'
+        r = requests.get(url, allow_redirects=True)
+        open(self.champ + '.png', 'wb').write(r.content)
 
-def searchChamp(Champion):
-    buttonBox = (None, None, None, None)
-    while not all(buttonBox):
+    def resizeAssets(self):
+        imageFile = self.champ + '.png'
+        im1 = Image.open(imageFile)
+        width = 64
+        height = 64
+        im2 = im1.resize((width, height), Image.NEAREST)
+        ext = '.jpg'
+        im2.save(self.champ + ext)
+
+    def clean(self):
+        if os.path.exists(self.champ + '.jpg') or os.path.exists(self.champ + '.png'):
+            os.remove(self.champ + '.jpg')
+            os.remove(self.champ + '.png')
+        else:
+            print("The file does not exist")
+
+class champSelect:
+    def __init__(self, champ, role):
+        self.champ = champ
+        self.role  = role
+
+    def clickImage(self):
+        imgpath = self.champ + '.jpg'
+        time.sleep(0.1)
+        buttonBox = (None, None, None, None)
+        while not all(buttonBox):
+            print(buttonBox, imgpath)
+            buttonBox = pyautogui.locateOnScreen(imgpath, grayscale=False, confidence=0.6)
+            if buttonBox == None:
+                buttonBox = (None, None, None, None)
         print(buttonBox)
-        buttonBox = pyautogui.locateOnScreen('searchbar.bmp', grayscale=True)
-        if buttonBox == None:
-            buttonBox = (None, None, None, None)
-    pyautogui.click(buttonBox)
-    pyautogui.typewrite(Champion)
+        buttonpoint = pyautogui.center(buttonBox)
+        pyautogui.click(buttonpoint, button='left')
 
-def selectRoleBP(role):
-    buttonBox = (None, None, None, None)
-    while not all(buttonBox):
-        print(buttonBox)
-        buttonBox = pyautogui.locateOnScreen('chat.jpg', grayscale=True)
-        if buttonBox == None:
-            buttonBox = (None, None, None, None)
+        print(buttonpoint)
+
+    def searchChamp(self):
+        buttonBox = (None, None, None, None)
+        while not all(buttonBox):
+            print(buttonBox)
+            buttonBox = pyautogui.locateOnScreen('searchbar.bmp', grayscale=True)
+            if buttonBox == None:
+                buttonBox = (None, None, None, None)
         pyautogui.click(buttonBox)
-        pyautogui.typewrite(role)
-        pyautogui.press('enter')
+        pyautogui.typewrite(self.champ)
 
-def dlSquareAssets(Champion):
-    Champion = Champion.capitalize()
-    url = 'http://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/' + Champion + '.png'
-    r = requests.get(url, allow_redirects=True)
-    open(Champion + '.png', 'wb').write(r.content)
+    def selectRoleBP(self):
+        buttonBox = (None, None, None, None)
+        while not all(buttonBox):
+            print(buttonBox)
+            buttonBox = pyautogui.locateOnScreen('chat.jpg', grayscale=True)
+            if buttonBox == None:
+                buttonBox = (None, None, None, None)
+            pyautogui.click(buttonBox)
+            pyautogui.typewrite(self.role)
+            pyautogui.press('enter')
 
-def resizeAssets(Asset, Champion):
-    imageFile = Asset
-    im1 = Image.open(imageFile)
-    width = 64
-    height = 64
-    im2 = im1.resize((width, height), Image.NEAREST)
-    ext = '.jpg'
-    im2.save(Champion + ext)
 
-def clean(Champion):
-    if os.path.exists(Champion + '.jpg') or os.path.exists(Champion + '.png') :
-        os.remove(Champion + '.jpg')
-        os.remove(Champion + '.png')
-    else:
-        print("The file does not exist")
+
+
+
 
 def selectChamp():
-    Champion = input()
-    dlSquareAssets(Champion)
-    resizeAssets(Champion + '.png', Champion)
     searchChamp(Champion)
     clickImage(Champion + '.jpg')
     clean(Champion)
@@ -76,9 +88,23 @@ def selectChamp():
     #clickImage('lockin.bmp')
 
 
-selectChamp()
+#selectChamp()
+
+input = input()
+
+def fetch(input):
+    champ = championAssets(input)
+    champ.dlSquareAssets()
+    champ.resizeAssets()
+    time.sleep(0.1)
+    #champ.clean()
 
 
+def select(input):
+    pick = champSelect(input)
+    #pick.searchChamp()
+    pick.clickImage()
+    pick.selectRoleBP()
 
-
-
+fetch(input)
+select(input)
