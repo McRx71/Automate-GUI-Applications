9412145b22549cc0689ff35df02fd2f2206e246e
Chris <dragon707@gmx.de>
2020-08-16 18:56:32 +0200

    Cleaned up the menu

--- SR/menu.py
+++ SR/menu.py
@@ -9,32 +9,33 @@ class menu:
         print('1) Start script')
         print('2) Close')
         choice = input('Select: 1 or 2: ')
-        if choice == '2':
-            exit()
-        elif choice == '1':
+        if choice == '1':
             choice = True
-        else:
+        elif choice == '2':
             choice = False
-        if choice == True:
-            while choice == True:
+        return choice
+
+
+
+    def selectScreenLogic(self):
+        if self.selectScreen() == True:
+            while self.selectScreen() == True:
                 input_champ = input('select your champion: 1')
                 input_role = input('select your role[Not working yet]: ')
                 action = actions(input_champ, input_role)
                 action.fetch()
                 action.select()
-                choice = input('Select another champ?')
-        else:
+                Main()
+        elif self.selectScreen() == False:
             exit()
-
-
-
-
+        else:
+            Main()
 
 
 
 def Main():
     m = menu(None, None)
     m.selectScreen()
-
+    m.selectScreenLogic()
 
 Main()
