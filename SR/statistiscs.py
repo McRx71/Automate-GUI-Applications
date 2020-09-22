d7895042d2a9ce969dd18fec1b45766beb82315e
McRx71 <50086322+McRx71@users.noreply.github.com>
2020-09-22 15:41:17 +0200

    calc wr and get matchlist

--- SR/statistiscs.py
+++ SR/statistiscs.py
@@ -1,11 +1,45 @@
 from riotwatcher import LolWatcher, ApiError
 import pandas as pd
+class statistics:
 
-# golbal variables
-api_key = ''
-watcher = LolWatcher(api_key)
-my_region = 'euw1'
+    def __init__(self, me, watcher, my_region, my_ranked_stats):
+        self.me = me
+        self.watcher = watcher
+        self.my_region = my_region
+        self.my_ranked_stats = my_ranked_stats
 
-me = watcher.summoner.by_name(my_region, '')
+    def connectAPI(self):
+        # golbal variables
+        api_key = 'RGAPI-f8dec745-34c7-49a9-a949-e15400b3df41'
+        self.watcher = LolWatcher(api_key)
+        self.my_region = 'euw1'
 
-my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
+    def getSummonerStats(self,usrname):
+        self.me = self.watcher.summoner.by_name(self.my_region, usrname)
+        self.my_ranked_stats = self.watcher.league.by_summoner(self.my_region, self.me['id'])
+        print(self.my_ranked_stats)
+
+    def wr(self):
+        #0 = SoloQ 1 = Flex
+        mrs = self.my_ranked_stats[0]
+        print(mrs['queueType'])
+        soloQ_insgesamt = int(mrs['wins']) + int(mrs['losses'])
+        print('Spiele insgesamt ' + str(soloQ_insgesamt))
+        winrate = (int(mrs['wins']) / soloQ_insgesamt) * 100 #Anzahl der Siege durch die gesamten Spiele
+
+        print(winrate)
+    def test(self):
+        all_matchlist = self.watcher.match.matchlist_by_account(self.my_region, self.me['accountId'])
+        print(all_matchlist)
+        print(type(all_matchlist))
+        matchlist = all_matchlist['matches']
+
+
+def Main():
+    stats = statistics(None, None, None, None)
+    stats.connectAPI()
+    stats.getSummonerStats(str(input('Enter your Summonername: ')))
+    stats.test()
+    #stats.wr()
+
+Main()
